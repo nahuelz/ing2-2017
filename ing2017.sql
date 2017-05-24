b4c9fa4a00aecfbeb57e87ff74c83c863ba1ef88
Nahuel Zanelli <nahuel.zanelli@hotmail.com>
2017-05-24 10:24:18 -0300

    Add files via upload

--- ing2017.sql
+++ ing2017.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 12-05-2017 a las 18:38:15
+-- Tiempo de generación: 24-05-2017 a las 15:26:08
 -- Versión del servidor: 10.1.21-MariaDB
--- Versión de PHP: 7.1.2
+-- Versión de PHP: 7.1.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -43,6 +43,34 @@ INSERT INTO `categoria` (`id`, `nombre`, `habilitada`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `comentario`
+--
+
+CREATE TABLE `comentario` (
+  `id` int(11) NOT NULL,
+  `idUsuario` int(11) NOT NULL,
+  `idFavor` int(11) NOT NULL,
+  `comentario` varchar(255) COLLATE utf8_spanish_ci NOT NULL,
+  `respuesta` varchar(255) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `nombreUsuario` varchar(255) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `fecha` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `comentario`
+--
+
+INSERT INTO `comentario` (`id`, `idUsuario`, `idFavor`, `comentario`, `respuesta`, `nombreUsuario`, `fecha`) VALUES
+(19, 2, 1, ' hola', 'chau', 'pepe', '2017-05-24 01:17:06'),
+(20, 2, 1, ' asd', 'chau2', 'pepe', '2017-05-24 01:22:29'),
+(21, 2, 1, ' asfaf', NULL, 'pepe', '2017-05-24 01:23:27'),
+(22, 2, 1, ' asfaf', NULL, 'pepe', '2017-05-24 01:25:08'),
+(23, 2, 1, ' asfaf', NULL, 'pepe', '2017-05-24 01:25:36'),
+(24, 1, 4, ' Hola', NULL, 'Admin', '2017-05-24 02:08:45');
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `favor`
 --
 
@@ -63,7 +91,32 @@ CREATE TABLE `favor` (
 --
 
 INSERT INTO `favor` (`id`, `usuario_id`, `titulo`, `descripcion`, `categoria`, `localidad`, `fecha_publicacion`, `cerrada`, `imagen`) VALUES
-(10, 1, 'asdasdasfaf', 'asfasfasf', 1, 'Azul', '2017-05-12', 0, '17862503_1636126096416315_1315970996329994303_n.jpg');
+(1, 1, 'ggggggggggggggg', 'ggggggggggggggg', 1, 'Azul', '2017-05-12', 0, ''),
+(2, 1, 'asd', 'asd', 1, 'Azul', '2017-05-12', 0, ''),
+(3, 1, 'un titulo', 'lorem impsu', 2, 'Azul', '2017-05-24', 0, ''),
+(4, 2, 'NUEVO', 'NUEVO', 1, 'Azul', '2017-05-24', 0, '');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `postulacion`
+--
+
+CREATE TABLE `postulacion` (
+  `id` int(11) NOT NULL,
+  `idFavor` int(11) NOT NULL,
+  `idUsuario` int(11) NOT NULL,
+  `estado` varchar(1) COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `postulacion`
+--
+
+INSERT INTO `postulacion` (`id`, `idFavor`, `idUsuario`, `estado`) VALUES
+(1, 1, 2, 'E'),
+(2, 1, 1, 'E'),
+(3, 3, 1, 'E');
 
 -- --------------------------------------------------------
 
@@ -88,7 +141,9 @@ CREATE TABLE `usuario` (
 --
 
 INSERT INTO `usuario` (`id`, `nombre`, `apellido`, `email`, `password`, `telefono`, `creditos`, `esAdmin`, `habilitado`) VALUES
-(1, 'Admin', 'Admin', 'admin@admin', 'admin', '123456789', '999', '1', 1);
+(1, 'Admin', 'Admin', 'admin@admin', 'admin', '123456789', '999', '1', 0),
+(2, 'pepe', 'pepe', 'pepe@pepe', 'pepe', '123', '1', '0', 0),
+(3, 'pepa', 'pepa', 'pepa@pepa', 'pepa', '123', '1', '0', 1);
 
 --
 -- Índices para tablas volcadas
@@ -101,9 +156,21 @@ ALTER TABLE `categoria`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indices de la tabla `comentario`
+--
+ALTER TABLE `comentario`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indices de la tabla `favor`
 --
 ALTER TABLE `favor`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `postulacion`
+--
+ALTER TABLE `postulacion`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -122,15 +189,25 @@ ALTER TABLE `usuario`
 ALTER TABLE `categoria`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
+-- AUTO_INCREMENT de la tabla `comentario`
+--
+ALTER TABLE `comentario`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+--
 -- AUTO_INCREMENT de la tabla `favor`
 --
 ALTER TABLE `favor`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT de la tabla `postulacion`
+--
+ALTER TABLE `postulacion`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
