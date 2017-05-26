aae7202f1bbe6cfcc710a72666b1210381f1ef07
Nahuel <nahuel.zanelli@homail.com>
2017-05-26 16:37:18 -0300

    VALIDACIONES EN EL REGISTRO

---
+++ templates/js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    var caracteres = /^[a-zA-ZáéíóúàèìòùÀÈÌÒÙÁÉÍÓÚñÑüÜ_\s]+$/;
+    var numeros = /^[0-9]+$/;
+    rows = 2;
+
+
+    $(".usuario").click(function (){
+        $(".error").remove();
+        if( $("#nombre").val().length < 4 || !caracteres.test($("#nombre").val())) {
+            $("#nombre").focus().after("<p class='error'>Ingrese un nombre valido</p>");
+            return false;  
+        }
+        if( $("#apellido").val().length < 4 || !caracteres.test($("#apellido").val())) {
+            $("#apellido").focus().after("<p class='error'>Ingrese un apellido valido</p>");
+            return false;  
+        }
+        if( $("#telefono").val().length < 7 || !numeros.test($("#telefono").val())) {
+            $("#telefono").focus().after("<p class='error'>Ingrese un telefono valido</p>");
+            return false;  
+        }
+    });
+});
+
