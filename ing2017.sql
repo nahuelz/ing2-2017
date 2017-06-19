500e6d44247b0e4aea0156af5a91841b8532f49e
Nahuel <nahuel.zanelli@homail.com>
2017-06-19 06:27:07 -0300

    Base de datos

--- ing2017.sql
+++ ing2017.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 30-05-2017 a las 05:15:55
+-- Tiempo de generación: 19-06-2017 a las 11:26:15
 -- Versión del servidor: 10.1.21-MariaDB
 -- Versión de PHP: 7.1.1
 
@@ -19,6 +19,20 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `ing2017`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `calificacion`
+--
+
+CREATE TABLE `calificacion` (
+  `id` int(11) NOT NULL,
+  `comentario` varchar(255) COLLATE utf8_spanish_ci NOT NULL,
+  `idUsuario` int(11) NOT NULL,
+  `idFavor` int(11) NOT NULL,
+  `calificacion` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 -- --------------------------------------------------------
 
@@ -88,7 +102,13 @@ INSERT INTO `creditos` (`id`, `usuarioId`, `precioUnitario`, `cantidad`, `fecha`
 (10, 1, 20, 1241, '2017-05-28'),
 (11, 1, 20, 1, '2017-05-28'),
 (12, 1, 20, 1, '2017-05-28'),
-(13, 1, 20, 1, '2017-05-28');
+(13, 1, 20, 1, '2017-05-28'),
+(14, 1, 20, -3000, '2017-05-30'),
+(15, 1, 20, 1, '2017-05-30'),
+(16, 1, 20, 760, '2017-05-30'),
+(17, 1, 20, 1, '2017-05-30'),
+(18, 1, 20, 10, '2017-06-06'),
+(19, 1, 20, 10, '2017-06-17');
 
 -- --------------------------------------------------------
 
@@ -105,15 +125,19 @@ CREATE TABLE `favor` (
   `localidad` varchar(255) COLLATE utf8_spanish_ci NOT NULL,
   `fecha_publicacion` date NOT NULL,
   `estado` varchar(1) COLLATE utf8_spanish_ci NOT NULL,
-  `imagen` varchar(255) COLLATE utf8_spanish_ci DEFAULT NULL
+  `imagen` varchar(255) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `idUsuarioAceptado` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
 -- Volcado de datos para la tabla `favor`
 --
 
-INSERT INTO `favor` (`id`, `usuario_id`, `titulo`, `descripcion`, `categoria`, `localidad`, `fecha_publicacion`, `estado`, `imagen`) VALUES
-(17, 1, 'unTitulo', 'UnaDescripcion', 1, 'Azul', '2017-05-30', 'A', 'UMLTPPromocionCORREGIDO.png');
+INSERT INTO `favor` (`id`, `usuario_id`, `titulo`, `descripcion`, `categoria`, `localidad`, `fecha_publicacion`, `estado`, `imagen`, `idUsuarioAceptado`) VALUES
+(29, 1, 'ASDSADSAD', '  ASDASFASFAFAF', 1, 'Laprida', '2017-06-17', 'A', '', NULL),
+(30, 1, '222222', '  22222', 2, 'Olavarria', '2017-06-17', 'A', '', NULL),
+(31, 1, 'FAFAFA', '      GGGGGGGGGG    ', 2, 'Bolivar', '2017-06-17', 'A', '', NULL),
+(32, 1, 'AAAA', '          sdgsdgsdg        ', 1, 'Azul', '2017-06-17', 'A', 'mudanza.jpg', NULL);
 
 -- --------------------------------------------------------
 
@@ -128,15 +152,6 @@ CREATE TABLE `postulacion` (
   `estado` varchar(1) COLLATE utf8_spanish_ci NOT NULL,
   `comentario` varchar(255) COLLATE utf8_spanish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
-
---
--- Volcado de datos para la tabla `postulacion`
---
-
-INSERT INTO `postulacion` (`id`, `idFavor`, `idUsuario`, `estado`, `comentario`) VALUES
-(4, 17, 2, 'E', 'hola'),
-(5, 17, 3, 'E', 'chau'),
-(6, 17, 8, 'E', 'asdf');
 
 -- --------------------------------------------------------
 
@@ -158,6 +173,29 @@ INSERT INTO `precio` (`precio`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `reputacion`
+--
+
+CREATE TABLE `reputacion` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(255) COLLATE utf8_spanish_ci NOT NULL,
+  `inicio` int(11) NOT NULL,
+  `fin` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `reputacion`
+--
+
+INSERT INTO `reputacion` (`id`, `nombre`, `inicio`, `fin`) VALUES
+(44, 'Muy Baja', -1000, -501),
+(45, 'Baja', -500, -1),
+(46, 'Alta', 0, 499),
+(47, 'Muy Alta', 500, 999);
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `usuario`
 --
 
@@ -170,6 +208,7 @@ CREATE TABLE `usuario` (
   `telefono` varchar(25) COLLATE utf8_spanish_ci NOT NULL,
   `localidad` varchar(255) COLLATE utf8_spanish_ci DEFAULT NULL,
   `creditos` varchar(8) COLLATE utf8_spanish_ci NOT NULL,
+  `puntos` int(11) NOT NULL,
   `esAdmin` varchar(1) COLLATE utf8_spanish_ci NOT NULL,
   `habilitado` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
@@ -178,19 +217,25 @@ CREATE TABLE `usuario` (
 -- Volcado de datos para la tabla `usuario`
 --
 
-INSERT INTO `usuario` (`id`, `nombre`, `apellido`, `email`, `password`, `telefono`, `localidad`, `creditos`, `esAdmin`, `habilitado`) VALUES
-(1, 'Admin', 'Admin', 'admin@admin', 'admin', '123456789', 'Bolivar', '2241', '1', 0),
-(2, 'pepe', 'pepe', 'pepe@pepe', 'pepe', '123', 'Bolivar', '141', '0', 0),
-(3, 'pepa', 'pepa', 'pepa@pepa', 'pepa', '123456', 'Bolivar', '1', '0', 1),
-(4, '1234', '1233', 'hola@hola', '1234', '123', '', '1', '0', 1),
-(5, 'asdf', 'asdf', 'asd@asd', 'asdf', '1', '', '1', '0', 1),
-(6, 'pipi', 'pipi', 'pipi@pipi', 'pipi', '03034567', 'Olavarria', '1', '0', 1),
-(7, 'popo', 'popo', 'popo@popo', 'popo', '0303456', 'Azul', '1', '0', 1),
-(8, 'asdf', 'asdf', 'asdf@asdf', 'asdf', '1234567', 'Tandil', '1', '0', 1);
+INSERT INTO `usuario` (`id`, `nombre`, `apellido`, `email`, `password`, `telefono`, `localidad`, `creditos`, `puntos`, `esAdmin`, `habilitado`) VALUES
+(1, 'Admin', 'Admin', 'admin@admin', 'admin', '123456789', 'Bolivar', '38', 2, '1', 0),
+(2, 'pepe', 'pepe', 'pepe@pepe', 'pepe', '123', 'Bolivar', '140', 0, '0', 0),
+(3, 'pepa', 'pepa', 'pepa@pepa', 'pepa', '123456', 'Bolivar', '1', 0, '0', 1),
+(4, '1234', '1233', 'hola@hola', '1234', '123', '', '1', 0, '0', 1),
+(5, 'asdf', 'asdf', 'asd@asd', 'asdf', '1', '', '1', 0, '0', 1),
+(6, 'pipi', 'pipi', 'pipi@pipi', 'pipi', '03034567', 'Olavarria', '1', 0, '0', 1),
+(7, 'popo', 'popo', 'popo@popo', 'popo', '0303456', 'Azul', '1', 0, '0', 1),
+(8, 'asdf', 'asdf', 'asdf@asdf', 'asdf', '1234567', 'Tandil', '1', 0, '0', 1);
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `calificacion`
+--
+ALTER TABLE `calificacion`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `categoria`
@@ -223,6 +268,12 @@ ALTER TABLE `postulacion`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indices de la tabla `reputacion`
+--
+ALTER TABLE `reputacion`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indices de la tabla `usuario`
 --
 ALTER TABLE `usuario`
@@ -232,6 +283,11 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
+--
+-- AUTO_INCREMENT de la tabla `calificacion`
+--
+ALTER TABLE `calificacion`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT de la tabla `categoria`
 --
@@ -246,17 +302,22 @@ ALTER TABLE `comentario`
 -- AUTO_INCREMENT de la tabla `creditos`
 --
 ALTER TABLE `creditos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 --
 -- AUTO_INCREMENT de la tabla `favor`
 --
 ALTER TABLE `favor`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 --
 -- AUTO_INCREMENT de la tabla `postulacion`
 --
 ALTER TABLE `postulacion`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `reputacion`
+--
+ALTER TABLE `reputacion`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
 --
 -- AUTO_INCREMENT de la tabla `usuario`
 --
