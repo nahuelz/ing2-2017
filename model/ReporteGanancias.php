49144fc54bb94751e7a9342ba80b5b82897e6141
brvalejandro <atp_pro@hotmail.com>
2017-06-24 22:44:28 -0300

    Reporte ganancias arreglado

--- model/ReporteGanancias.php
+++ model/ReporteGanancias.php
@@ -7,6 +7,10 @@
  */
  class ReporteGanancia extends PDORepository {
  	private static $instance;
+    protected $nombre;
+    protected $apellido;
+    protected $email;
+    protected $fecha;
     protected $totalRecaudado;
     protected $creditosVendidos;
 
@@ -19,12 +23,43 @@
         return self::$instance;
     }
 
-    function __construct($totalRecaudado=null, $creditosVendidos=null){
-
+    function __construct($nombre=null, $apellido=null, $email=null, $fecha=null, $creditosVendidos=null, $totalRecaudado=null){
+        $this->nombre=$nombre;
+        $this->apellido=$apellido;
+        $this->email=$email;
+        $this->fecha=$fecha;
         $this->totalRecaudado = $totalRecaudado;
-        $this->creditosVendidos = $creditosVendidos;
-        
+        $this->creditosVendidos = $creditosVendidos;    
         return $this;
+    }
+
+    public function setNombre($nombre){
+        $this->nombre = $nombre;
+    }
+
+    public function getNombre(){
+        return $this->nombre;
+    }
+    public function setApellido($apellido){
+        $this->apellido = $apellido;
+    }
+
+    public function getApellido(){
+        return $this->apellido;
+    }
+    public function setEmail($email){
+        $this->email = $email;
+    }
+
+    public function getEmail(){
+        return $this->email;
+    }
+    public function setFecha($fecha){
+        $this->fecha = $fecha;
+    }
+
+    public function getFecha(){
+        return $this->fecha;
     }
 
     public function setTotalRecaudado($totalRecaudado){
@@ -46,13 +81,13 @@
     public function reporteGanancias($fechaInicial, $fechaFinal) {
         $mapper = function($row) {
 
-            $resource = new ReporteGanancia($row['totalRecaudado'], $row['creditosVendidos']);
+            $resource = new ReporteGanancia($row['nombre'],$row['apellido'],$row['email'],$row['fecha'], $row['creditosVendidos'],$row['totalRecaudado']);
             return $resource;
         };
 
-        $sql="SELECT SUM(precioUnitario) as totalRecaudado, SUM(cantidad) as creditosVendidos FROM creditos WHERE fecha BETWEEN ? AND ? ";
+        $sql="SELECT usuario.nombre, usuario.apellido, usuario.email, creditos.fecha, creditos.cantidad as creditosVendidos, (creditos.cantidad * creditos.precioUnitario) as totalRecaudado FROM creditos INNER JOIN usuario ON creditos.usuarioId=usuario.id WHERE creditos.fecha BETWEEN ? AND ? ORDER BY totalRecaudado DESC";
         $values=[$fechaInicial,$fechaFinal];
         $answer = $this->queryList($sql, $values, $mapper);
-        return ($answer[0]);
+        return ($answer);
     }
  }
